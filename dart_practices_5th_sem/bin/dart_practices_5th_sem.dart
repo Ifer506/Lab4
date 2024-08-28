71d9cb67aab904ebac093d19dbb847c0a0ad1b91
Ifer506 <shakyarajsumedha@gmail.com>
2024-08-28 13:28:38 +0545

    Done

--- dart_practices_5th_sem/bin/dart_practices_5th_sem.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_practices_5th_sem/dart_practices_5th_sem.dart' as dart_practices_5th_sem;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_practices_5th_sem.calculate()}!');
-}
--- dart_practices_5th_sem/test/dart_practices_5th_sem_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_practices_5th_sem/dart_practices_5th_sem.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
